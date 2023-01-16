f93e02ad2fc9dc50b4ee41802d769f5108e00e9d
maryane-castro <122397850+maryane-castro@users.noreply.github.com>
2023-01-16 19:44:41 -0300

    js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Funcionando hehe!!");
+var currentImgIndex=1;
+
+var ImgSrcArray = [ //caminho das suas imgs aqui
+"margo2_rose.jpg",
+"sofar.gif"
+];
+
+function trocar(){
+
+  if(currentImgIndex == ImgSrcArray.length) //reseta quando o contatador for igual ao tamanho da array e volta a 1° img
+  {
+    currentImgIndex=0;
+  }
+  document.getElementById("image").src=ImgSrcArray[currentImgIndex]; //altera a img do elemento "agni" de acordo com o indice
+    currentImgIndex++; // incrementa a nossa referencia
+
+}
