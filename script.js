7c12074540638b2a9db9bc123460e90d0a1b1076
maryane-castro <122397850+maryane-castro@users.noreply.github.com>
2023-01-17 13:17:42 -0300

    retirei svg
    tirei o svg e coloquei imagem png do 360 e do X

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
-var currentImgIndex=1;
 
+
+var currentImgIndex=1;
+var sv=0;
 var ImgSrcArray = [ //caminho das suas imgs aqui
 "margo2_rose.jpg",
 "sofar.gif"
 ];
 
+var ImgArray= [
+  "Vector.png",
+  "Vector (Stroke).png",
+
+];
+
 function trocar(){
+
+ 
 
   if(currentImgIndex == ImgSrcArray.length) //reseta quando o contatador for igual ao tamanho da array e volta a 1° img
   {
+    
     currentImgIndex=0;
+    sv=0;
   }
+  document.getElementById("360").src=ImgArray[sv];
   document.getElementById("image").src=ImgSrcArray[currentImgIndex];
-  document.getElementById("sgvt").sv//altera a img do elemento "agni" de acordo com o indice
-    currentImgIndex++; // incrementa a nossa referencia
-
+    currentImgIndex++; 
+    sv++;
 }
+
+
+
 
